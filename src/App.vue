be047756a3d389ed63ceaccd6494470a45926195
emelkaraman <91911554+emelkaraman@users.noreply.github.com>
2022-03-30 17:32:41 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="nav">
+    
+  </div>
+  <router-view />
+</template>
+
+<style>
+#app {
+ 
+}
+
+#nav {
+  
+}
+
+#nav a {
+ 
+}
+
+#nav a.router-link-exact-active {
+  
+}
+</style>
